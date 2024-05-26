713e6c6d612fcafbbfaf5341bbf126aaec1c3afc
jessica <jessboll16@gmail.com>
2024-05-26 17:33:49 -0500

    created

---
+++ Working/Math.java
@@ -0,0 +1,46 @@
+package Working;
+
+public class Math {
+public static void main(String[] args) {
+    //Addition
+    int x=8;
+    int y=10;
+    System.out.println(x+y);
+
+    //Variables
+    double price1=7.99;
+    double price2=8.99;
+    double sum = price1+price2;
+    System.out.println(sum);
+
+     //Subtraction
+    int startpear= 17;
+    int endpear=3;
+    int take= startpear-endpear;
+    System.out.println(take);
+
+     //Multiplication
+    int length=7;
+    int width=4;
+    int area=length * width;
+    System.out.println(area);
+
+    //Division
+    double price=18.99;
+    int money=30;
+    double buy=money/price;
+    System.out.println(buy);
+
+    //Modulo
+    int total=20;
+    double res= total %5.99;
+    System.out.println(res);
+
+    //Strings
+    String firstname="Jessica";
+    String lastname=" Boll";
+    String fullname=firstname+lastname;
+    System.out.println(fullname);
+}
+
+}
