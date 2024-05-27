31b7f09a431e9e76676a4bb8d382ce47fb20aa85
jessica <jessboll16@gmail.com>
2024-05-26 19:08:03 -0500

    Create practiceproblems.java

---
+++ Working/practiceproblems.java
@@ -0,0 +1,12 @@
+package Working;
+
+public class practiceproblems {
+public static void main(String[] args) {
+    //Write a Java program to print 'Hello' on screen and your name on a separate line.
+    String hello="Hello";
+    String name="Jessica";
+    System.out.println(hello);
+    System.out.println(name);
+    
+}
+}
