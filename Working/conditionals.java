0bc41bc7192bb9eb0619bbf23f531313ceb5ed27
jessica <jessboll16@gmail.com>
2024-05-27 23:32:38 -0500

    kljnb

---
+++ Working/conditionals.java
@@ -0,0 +1,12 @@
+package Working;
+
+public class conditionals {
+public static void main(String[] args) {
+    int age = 11;
+    if (age <12) {
+        System.out.println("Too Young");
+    } else {
+        System.out.println("Welcome");
+    }
+}
+}
