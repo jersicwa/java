8440c6a9aca353bac05316057add8d1c560219af
jessica <jessboll16@gmail.com>
2024-05-27 23:12:39 -0500

    ugh

--- Working/input.java
+++ Working/input.java
@@ -2,11 +2,11 @@ package Working;
 import java.util.Scanner;
 public class input {
 public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    String nick = sc.nextLine();
-    int age = sc.nextInt();
-    System.out.println(nick+":"+age);
-
-    
+    Scanner in=new Scanner(System.in);
+    System.out.print("Input first number: ");
+    int num1 = in.nextInt();
+    System.out.print("Input second number: ");
+    int num2 = in.nextInt();
+    System.out.println(num1+num2+"="+num1*num2);
 }
 }
--- Working/practiceinput.java
+++ Working/practiceinput.java
@@ -2,7 +2,7 @@ package Working;
 import java.util.Scanner;
 public class practiceinput {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         Int one = sc.nextInt();
         Int two = sc.nextInt();
         Int sum = one*two;
